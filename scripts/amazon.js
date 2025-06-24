b75d1cd03f9eab0d7f5b51fbe91907598dfd46e2
MjComia <markjoel.comia@tup.edu.ph>
2025-06-24 22:35:56 +0800

    finished add to cart button

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -51,7 +51,7 @@ products.forEach((product) => {
           </div>
 
           <button class="add-to-cart-button button-primary
-          js-add-to-cart" data-product-name = "${product.name}">
+          js-add-to-cart" data-product-id = "${product.id}">
             Add to Cart
           </button>
         </div>`;
@@ -62,14 +62,26 @@ products.forEach((product) => {
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
   button.addEventListener('click', () => {
-    const productName = button.dataset.productName;
-    cart.push({
+    const productId = button.dataset.productId;
 
-      
-
-      productName: productName,
-      quantity: 1
+    let matchingItem;
+    cart.forEach((item) => {
+      if (productId === item.productId){
+        matchingItem = item;
+      }
     });
+
+    if (matchingItem){
+      matchingItem.quantity++;
+    }else {
+        cart.push({
+          productId: productId,
+          quantity: 1
+    });
+
+    } 
+
+
     console.log(cart);
   })
 
